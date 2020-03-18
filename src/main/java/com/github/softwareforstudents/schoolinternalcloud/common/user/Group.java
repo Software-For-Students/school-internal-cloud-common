29fe66c744ef880b5b4cd2ce468c159b96a536fe
JannesForTec <jannes@opicht.de>
2020-03-18 22:48:23 +0100

    add annotations to user common classes

--- src/main/java/com/github/softwareforstudents/schoolinternalcloud/common/user/Group.java
+++ src/main/java/com/github/softwareforstudents/schoolinternalcloud/common/user/Group.java
@@ -2,37 +2,42 @@ package com.github.softwareforstudents.schoolinternalcloud.common.user;
 
 import java.util.Set;
 
+import com.github.softwareforstudents.schoolinternalcloud.common.annotations.Immutable;
+import com.github.softwareforstudents.schoolinternalcloud.common.annotations.NotNull;
+
 public class Group {
 
     private String name;
     private Set<User> users;
 
-    public Group(String name, Set<User> users) {
+    public Group(@NotNull String name, @NotNull Set<User> users) {
         this.name = name;
         this.users = users;
     }
 
+    @Immutable
     public String getName() {
-        return name;
+        return String.valueOf(name);
     }
 
-    public void setName(String name) {
+    public void setName(@NotNull String name) {
         this.name = name;
     }
 
+    @Immutable
     public Set<User> getUsers() {
-        return users;
+        return Set.copyOf(users);
     }
 
-    public void setUsers(Set<User> users) {
+    public void setUsers(@NotNull Set<User> users) {
         this.users = users;
     }
 
-    public void addUser(User user) {
+    public void addUser(@NotNull User user) {
         users.add(user);
     }
 
-    public void removeUser(User user) {
+    public void removeUser(@NotNull User user) {
         users.remove(user);
     }
 }
--- src/main/java/com/github/softwareforstudents/schoolinternalcloud/common/user/UserDetails.java
+++ src/main/java/com/github/softwareforstudents/schoolinternalcloud/common/user/UserDetails.java
@@ -2,13 +2,15 @@ package com.github.softwareforstudents.schoolinternalcloud.common.user;
 
 import java.time.LocalDate;
 
+import com.github.softwareforstudents.schoolinternalcloud.common.annotations.NotNull;
+
 //TODO stub class -> add details
 
 public class UserDetails implements Cloneable {
 
     private LocalDate birthdayDate;
 
-    public UserDetails(LocalDate birthdayDate) {
+    public UserDetails(@NotNull LocalDate birthdayDate) {
         this.birthdayDate = birthdayDate;
     }
 
